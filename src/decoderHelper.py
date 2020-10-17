235a49eac3b7b254579fec3fab6c8455d0662786
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2020-10-16 20:21:25 -0500

    Removed digits 1 and 0 from valid message
    1 and 0 do not correspond to characters on the phone keypad.

--- src/decoderHelper.py
+++ src/decoderHelper.py
@@ -1,7 +1,7 @@
 
 def isMessageValid(message):
     for character in message:
-        if((not character.isdigit()) and (character != '-')):
+        if((character != '-') and (character != '2') and (character != '3') and (character != '4') and (character != '5') and (character != '6') and (character != '7') and (character != '8') and (character != '9')):
             return False
     return True
 
