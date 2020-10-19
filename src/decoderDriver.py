38c479c70ad22c6e445785f65a43f96dce3809d6
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2020-10-19 16:17:16 -0500

    Permutations now output to file

--- src/decoderDriver.py
+++ src/decoderDriver.py
@@ -7,4 +7,33 @@ def decodePhoneKeypadMessage():
 
     allWordPermutations = getAllWordPermutations(rawMessage)
 
-    printAllWordPermutations(allWordPermutations)
+    printPermutationsToFile(allWordPermutations, "word_possibilities.txt")
+
+
+def printPermutationsToFile(allWordPermutations, fileName):
+    file = open(fileName, "w")
+
+    currentWordNumber = 1
+    for wordPermutations in allWordPermutations:
+
+        totalPermutationsPossible = len(wordPermutations)
+        processedPermutations = 0
+
+        file.write(f"\t\tWORD {currentWordNumber}\n")
+
+        currentWordPermutationNumber = 1
+        for combination in wordPermutations:
+            file.write(str(currentWordPermutationNumber) +
+                       ":" + "".join(combination) + "\n")
+
+            processedPermutations = processedPermutations + 1
+
+            printProgressBar(processedPermutations, totalPermutationsPossible, prefix=(
+                "Writing Word " + str(currentWordNumber)), suffix="Complete", length=50)
+
+            currentWordPermutationNumber = currentWordPermutationNumber + 1
+        currentWordNumber = currentWordNumber + 1
+
+    print(f"Possible Words can be found in {fileName}")
+
+    file.close()
